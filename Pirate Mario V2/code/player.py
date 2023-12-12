20b90af4381ded94ff642228b64e8fa6f1cf8757
Yahya <yahyafparuk@gmail.com>
2023-12-12 13:49:57 +0200

    Ui + collisions and coins!

--- Pirate Mario V2/code/player.py
+++ Pirate Mario V2/code/player.py
@@ -1,7 +1,9 @@
 import pygame
 from support import import_folder
+from math import sin
+
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos,surface,create_jump_particles):
+    def __init__(self, pos,surface,create_jump_particles,change_health):
         super().__init__()
         self.import_character_assets()
         self.frame_index = 0
@@ -30,6 +32,12 @@ class Player(pygame.sprite.Sprite):
         self.on_left = False
         self.on_right = False
 
+        #health management
+        self.change_health  = change_health
+        self.invincible = False
+        self.invincibility_duration = 500
+        self.hurt_time = 0
+
     def import_character_assets(self):
         character_path = '../graphics/character/'
         self.animations = {'idle':[],'run':[],'jump':[],'fall':[]}
@@ -40,6 +48,7 @@ class Player(pygame.sprite.Sprite):
 
     def import_dust_run_particles(self):
         self.dust_run_particles = import_folder('../graphics/character/dust_particles/run')
+
     def animate(self):
         animation = self.animations[self.status]
 
@@ -54,6 +63,12 @@ class Player(pygame.sprite.Sprite):
         else:
             flipped_image = pygame.transform.flip(image,True,False)
             self.image = flipped_image
+
+        if self.invincible:
+            alpha = self.wave_value()
+            self.image.set_alpha(alpha)
+        else:
+            self.image.set_alpha(255)
 
         #set the rectangle
         if self.on_ground and self.on_right:
@@ -85,7 +100,6 @@ class Player(pygame.sprite.Sprite):
                 flipped_dust_particle = pygame.transform.flip(dust_particle,True,False)
                 self.display_surface.blit(flipped_dust_particle,pos)
 
-
     def get_input(self):
         keys = pygame.key.get_pressed()
 
@@ -98,7 +112,7 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.x = 0
 
-        if (keys[pygame.K_SPACE] or keys[pygame.K_w]) and self.on_ground:
+        if (keys[pygame.K_SPACE] or keys[pygame.K_w] or keys[pygame.K_UP]) and self.on_ground:
             self.jump()
             self.create_jump_particles(self.rect.midbottom)
 
@@ -120,8 +134,27 @@ class Player(pygame.sprite.Sprite):
     def jump(self):
         self.direction.y = self.jump_speed
 
+    def get_damage(self):
+        if not self.invincible:
+         self.change_health(-10)
+        self.invincible = True
+        self.hurt_time = pygame.time.get_ticks()
+
+    def invincibility_timer(self):
+        if self.invincible:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.hurt_time >= self.invincibility_duration:
+                self.invincible = False
+
+    def wave_value(self):
+        value = sin(pygame.time.get_ticks())
+        if value >= 0:return 255
+        else:return 0
+
     def update(self):
         self.get_input()
         self.get_status()
         self.animate()
         self.run_dust_animation()
+        self.invincibility_timer()
+
